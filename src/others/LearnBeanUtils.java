ef8cf00df4f193f832d552250f0c88054566ec95
xinil502 <1223176327@qq.com>
2020-09-26 22:10:27 +0800

    BeanUtils学习

---
+++ src/others/LearnBeanUtils.java
@@ -0,0 +1,24 @@
+package others;
+
+/**
+ *BeanUtils学习
+ * 1.Javabean介绍
+ *      1.要求∶
+ *          1．类必须被public修饰
+ *          2必须提供空参的构造器
+ *          3．成员变量必须使用private修饰
+ *          4．提供公共setter和getter方法
+ *      2.功能∶封装数据
+ *  2。概念:
+ *      成员变量:
+ *      属性:setter和getter方法截取后的产物
+ *          例如:getUsername() -->Username-->username
+ *  3．方法:
+ *      1. setProperty()
+ *      2. getProperty()
+ *      3.populate(object obj , Map map):
+ *          将map集合的键值对信息，封装到对应的JavaBean对象中
+ *          键做变量名，值做变量值。
+ */
+public class LearnBeanUtils {
+}
