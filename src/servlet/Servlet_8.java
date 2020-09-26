a490f78c56ccac2a52eef3978ce985f4afb0cabc
xinil502 <1223176327@qq.com>
2020-09-26 22:12:17 +0800

    重定向学习

---
+++ src/servlet/Servlet_8.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 重定向
+ */
+@WebServlet("/servlet_8")
+public class Servlet_8 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //访问servlet_8会跳转到servlet_8_1资源
+
+        /**
+         * response.setStatus(302);
+         * response.setHeader("location","/LearnWeb/servlet_8_1");
+         */
+
+        response.sendRedirect("http://www.baidu.com");
+
+    }
+}
