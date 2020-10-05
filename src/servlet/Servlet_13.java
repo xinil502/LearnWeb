8798db87fd52d10cfd7f98363df4031c25bef678
xinil502 <1223176327@qq.com>
2020-10-06 00:04:01 +0800

    Filter试用

--- src/servlet/Servlet_13.java
+++ src/servlet/Servlet_13.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 /**
  * Filter 使用
  */
-@WebFilter("/*")
+@WebFilter(value = "/*", dispatcherTypes = DispatcherType.REQUEST)
 public class Servlet_13 implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -16,8 +16,13 @@ public class Servlet_13 implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("过滤器filter_1正在执行中。。。");
+        //对request消息的拦截
+        System.out.println("过滤器filter_1正在过滤请求");
+
         filterChain.doFilter(servletRequest, servletResponse);//放行
+
+        //对response对象的增强。
+        System.out.println("过滤器filter_1正在过滤响应");
     }
 
     @Override
